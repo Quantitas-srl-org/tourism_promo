7e67eb95350544920b6ce1a0fbcdba041c4cc5a1
LC.D <lorenzo@cataldi.design>
2024-08-27 17:39:32 +0200

    setup header menu

--- assets/js/script.babel.js
+++ assets/js/script.babel.js
@@ -1,14 +1,41 @@
+/**
+ * DOM elements
+ */
+const menuToggleBtn = document.querySelector('#menu-toggle-btn'),
+  siteHeader = document.querySelector('.site-header'),
+  icon = menuToggleBtn.querySelector('span');
+
+/**
+ * Header
+ */
 const header = {
-  headerFunction: () => {
-    console.log('header works!')
+  siteMenuToggle: () => {
+    menuToggleBtn.addEventListener('click', () => {
+      siteHeader.classList.toggle('is-open');
+      if (siteHeader.classList.contains('is-open')) {
+        icon.innerHTML = 'close';
+        menuToggleBtn.setAttribute('aria-label', 'Chiudi il menù');
+      } else {
+        icon.innerHTML = 'menu';
+        menuToggleBtn.setAttribute('aria-label', 'Apri il menù');
+      }
+    })
+  },
+  keyPressEscToCloseMenu: () => {
+    document.addEventListener('keypress', (event) => {
+      if (event.key === 'Escape') {
+        siteHeader.classList.remove('is-open');
+      }
+      console.log(event.key)
+    })
   }
 }
 
-const components = {
+/*const components = {
   componentsFunction: () => {
     console.log('component works!')
   },
-}
+}*/
 
-header.headerFunction();
-components.componentsFunction();
+header.siteMenuToggle();
+header.keyPressEscToCloseMenu();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-const header={headerFunction:()=>{console.log("header works!")}},components={componentsFunction:()=>{console.log("component works!")}};header.headerFunction(),components.componentsFunction();
+/**
+ * DOM elements
+ */const menuToggleBtn=document.querySelector("#menu-toggle-btn"),siteHeader=document.querySelector(".site-header"),icon=menuToggleBtn.querySelector("span"),header={siteMenuToggle:()=>{menuToggleBtn.addEventListener("click",()=>{siteHeader.classList.toggle("is-open"),siteHeader.classList.contains("is-open")?(icon.innerHTML="close",menuToggleBtn.setAttribute("aria-label","Chiudi il men\xF9")):(icon.innerHTML="menu",menuToggleBtn.setAttribute("aria-label","Apri il men\xF9"))})},keyPressEscToCloseMenu:()=>{document.addEventListener("keypress",a=>{"Escape"===a.key&&siteHeader.classList.remove("is-open"),console.log(a.key)})}};/**
+ * Header
+ *//*const components = {
+  componentsFunction: () => {
+    console.log('component works!')
+  },
+}*/header.siteMenuToggle(),header.keyPressEscToCloseMenu();
