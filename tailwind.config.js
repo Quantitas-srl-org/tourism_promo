e7f694335d3fda9fd3c9124732dc49ed0703f513
LC.D <lorenzo@cataldi.design>
2024-08-27 08:04:11 +0200

    add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     '*.markdown'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Lato', 'Helvetica', 'sans-serif'],
+    },
+    colors: {
+      'black': '#030520',
+      'white': '#ffffff',
+      'false-white': '#FDFCF6',
+      'tan': '#EBE8E3',
+      'accent': '#3FFD6A',
+      'primary-dark': '#12275C', //'#13285E',
+      'primary': '#1157AD',
+      'secondary': '#00BD2B',
+      'secondary-dark': '#005536',
+    },
     extend: {},
   },
   plugins: [],
