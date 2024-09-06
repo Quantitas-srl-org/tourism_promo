7c87b16f76335092b93f4d7445879e6f4183b2b8
lorenzo-minsait <lcataldi@minsait.com>
2024-09-06 11:21:24 +0200

    add template compression and robots.txt plus refinement

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: ['./assets/js/scroll-timeline.js', './assets/js/script.webpack.js'],
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'script.js',
     path: path.resolve(__dirname, 'assets/js'),
